2ed9ecf3445b954eaeb99c04f80a9c544332b306
Tim Wilder <tmwilder@gmail.com>
2013-06-09 15:52:04 -0700

    Added simple unit tests to check response code
    1.Filled out simple unit tests to request each of our intended pages and check that the response comes back as 200.

--- pong_site/pong_app/tests.py
+++ pong_site/pong_app/tests.py
@@ -6,11 +6,65 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from django.test.client import Client
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class StatusOkTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+        
+    def test_index(self):
+        response = self.client.post("/index/")
+        self.assertEqual(response.status_code, 200)
+        
+    def test_enter_result(self):
+        response = self.client.post("/enter_result/")
+        self.assertEqual(response.status_code, 200)
+        
+    def test_make_player(self):
+        response = self.client.post("/make_player/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_make_team(self):
+        response = self.client.post("/make_team/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_make_league(self):
+        response = self.client.post("/make_league/")
+        self.assertEqual(response.status_code, 200)
+                                
+    def test_update_player(self):
+        response = self.client.post("/update_player/")
+        self.assertEqual(response.status_code, 200)
+    
+    def test_update_team(self):
+        response = self.client.post("/update_team/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_update_league(self):
+        response = self.client.post("/update_league/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_add_player_to_team(self):
+        response = self.client.post("/add_player_to_team/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_add_team_to_league(self):
+        response = self.client.post("/add_team_to_league/")
+        self.assertEqual(response.status_code, 200)
+                                                                                           
+    def test_team_profile(self):
+        response = self.client.post("/team_profile/")
+        self.assertEqual(response.status_code, 200)
+        
+    def test_player_profile(self):
+        response = self.client.post("/player_profile/")
+        self.assertEqual(response.status_code, 200)
+                
+    def test_standings(self):
+        response = self.client.post("/standings/")
+        self.assertEqual(response.status_code, 200)
+        
+    def test_match_profile(self):
+        response = self.client.post("/match_profile/")
+        self.assertEqual(response.status_code, 200)                
