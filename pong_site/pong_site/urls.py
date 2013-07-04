ecb6fd4275d8ee0832dc8a68172fe911299e4225
Justin Yan <justin.yan@redfin.com>
2013-07-04 14:59:48 -0700

    Fixing tabbed indents

--- pong_site/pong_site/urls.py
+++ pong_site/pong_site/urls.py
@@ -13,30 +13,30 @@ import pong_app.views.registration as registration
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-	url(r'^index/{0,1}.*$', misc.index),
-	url(r'^enter_result/{0,1}.*$', misc.enter_result),
-	url(r'^team_matches/(?P<team_id>\d+)/{0,1}.*$', misc.team_matches),
-	#Make
-	url(r'^make_user/{0,1}.*$', makes.make_user),
-	url(r'^make_team/{0,1}.*$', makes.make_team),
-	url(r'^make_league/{0,1}.*$', makes.make_league),
+    url(r'^index/{0,1}.*$', misc.index),
+    url(r'^enter_result/{0,1}.*$', misc.enter_result),
+    url(r'^team_matches/(?P<team_id>\d+)/{0,1}.*$', misc.team_matches),
+    #Make
+    url(r'^make_user/{0,1}.*$', makes.make_user),
+    url(r'^make_team/{0,1}.*$', makes.make_team),
+    url(r'^make_league/{0,1}.*$', makes.make_league),
     #Update
     url(r'^update_user/{0,1}$', updates.update_user),
     url(r'^update_team/{0,1}$', updates.update_team),
-	url(r'^update_team/(?P<team_id>\d+).*$', updates.update_team),
-	#Add
-	url(r'^add_team_to_league/{0,1}.*$', add_x_to_y.add_team_to_league),
-	url(r'^add_user_to_team/{0,1}.*$', add_x_to_y.add_user_to_team),
-	#Profiles
-	url(r'^team_profile/{0,1}$', profiles.team_profile),
-	url(r'^team_profile/(?P<team_id>\d+)/{0,1}$', profiles.team_profile),
+    url(r'^update_team/(?P<team_id>\d+).*$', updates.update_team),
+    #Add
+    url(r'^add_team_to_league/{0,1}.*$', add_x_to_y.add_team_to_league),
+    url(r'^add_user_to_team/{0,1}.*$', add_x_to_y.add_user_to_team),
+    #Profiles
+    url(r'^team_profile/{0,1}$', profiles.team_profile),
+    url(r'^team_profile/(?P<team_id>\d+)/{0,1}$', profiles.team_profile),
     url(r'^user_profile/{0,1}$', profiles.user_profile),
-	url(r'^user_profile/(?P<user_id>\d+).*$', profiles.user_profile),
-	url(r'^league_profile/(?P<league_id>\d+)/{0,1}$', profiles.league_profile),
-	#Authentication
+    url(r'^user_profile/(?P<user_id>\d+).*$', profiles.user_profile),
+    url(r'^league_profile/(?P<league_id>\d+)/{0,1}$', profiles.league_profile),
+    #Authentication
     (r'^accounts/login/$',  login),
-    (r'^accounts/logout/$', logout, {'next_page':'/index/'}),
+    (r'^accounts/logout/$', logout, {'next_page': '/index/'}),
     (r'^accounts/register/$', registration.register),
     #Test
-url(r'^test/{0,1}.*$', test.test)
+    url(r'^test/{0,1}.*$', test.test)
 )
